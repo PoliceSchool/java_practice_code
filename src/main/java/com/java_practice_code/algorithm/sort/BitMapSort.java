b71f2865b11da8d06d16d2e72bbaf6b526b474eb
lujingxiao <lujingxiao@sythealth.com>
2019-11-20 18:58:48 +0800

    添加注释

--- src/main/java/com/java_practice_code/algorithm/sort/BitMapSort.java
+++ src/main/java/com/java_practice_code/algorithm/sort/BitMapSort.java
@@ -1,15 +1,15 @@
 package com.java_practice_code.algorithm.sort;
 
 /**
- * description: bit-map排序算法
+ * description: bit-map排序算法,不过受限于int或者long的位数限制
  *
  * @author lujingxiao
  */
 public class BitMapSort {
     public static void main(String[] args) {
-        int[] arr = {4, 30, 7, 2, 5, 3, 12};
+        int[] arr = {4, 32, 7, 2, 5, 3, 12};
         int min = 0;
-        int bitMap = sort(arr);
+        long bitMap = sort(arr);
         while (bitMap != 0) {
             if (bitMap % 2 == 1) {
                 System.out.print(min + " ");
@@ -19,11 +19,11 @@ public class BitMapSort {
         }
     }
 
-    private static int sort(int[] arr) {
-        int bitMap = 0;
+    private static long sort(int[] arr) {
+        long bitMap = 0;
         for (int i : arr) {
             // 这一步将对应的比特位置为1；比如i为5的话，会将bitMap的第6位设置为1，如果为7则将bitMap的第8位设置为1
-            bitMap |= (1 << i);
+            bitMap |= (1L << i);
         }
         return bitMap;
     }
