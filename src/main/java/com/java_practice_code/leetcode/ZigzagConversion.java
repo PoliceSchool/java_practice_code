1f4cfdc7588cec5703527384b051f92bfd6e854e
lujingxiao <lujingxiao@sythealth.com>
2020-01-04 11:27:07 +0800

    添加leet算法题

---
+++ src/main/java/com/java_practice_code/leetcode/ZigzagConversion.java
@@ -0,0 +1,61 @@
+package com.java_practice_code.leetcode;
+
+/**
+ * description: Z字形变换：https://leetcode-cn.com/problems/zigzag-conversion/submissions/
+ * date: Created in 2020/1/4.
+ * 详解：按照给定的字符串画出行数为3、4、5的情况，分析字符串中每个字符位置下标数字，总结出规律即可。
+ * 3行时每个字符串位置的情况：
+ * 0   4   8
+ * 1 3 5 7 9  11
+ * 2   6   10
+ *
+ * 4行时每个字符串位置的情况：
+ * 0     6      12
+ * 1   5 7   11 13
+ * 2 4   8 10   14
+ * 3     9      15
+ *
+ * 5行时每个字符串位置的情况：
+ * 0       8          16
+ * 1     7 9       15 17
+ * 2   6   10   14    18
+ * 3 5     11 13      19
+ * 4       12         20
+ * @author lujingxiao
+ */
+public class ZigzagConversion {
+    public static void main(String[] args) {
+        ZigzagConversion zigzagConversion = new ZigzagConversion();
+        System.out.println(zigzagConversion.convert("LEETCODEISHIRING", 3));
+    }
+
+    private String convert(String s, int numRows) {
+        int index = 0, length = s.length(), up = 2 * numRows - 2, down = 0;
+        if (up == 0) {
+            return s;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (index < numRows) {
+            boolean flag = true;
+            int subIndex = index;
+            while (subIndex < length) {
+                if (down == 0 || down == up) {
+                    result.append(s.charAt(subIndex));
+                    subIndex += up;
+                    continue;
+                }
+                result.append(s.charAt(subIndex));
+                if (flag) {
+                    subIndex += (up - down);
+                    flag = false;
+                } else {
+                    subIndex += down;
+                    flag = true;
+                }
+            }
+            down += 2;
+            index++;
+        }
+        return result.toString();
+    }
+}
