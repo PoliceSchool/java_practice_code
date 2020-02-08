9814b818633147a8a62afaf50f103c9c4d28aa12
policeschool <policeschool@126.com>
2020-02-08 11:57:15 +0800

    添加死锁代码

---
+++ src/main/java/com/java_practice_code/algorithm/stack/MinStack.java
@@ -0,0 +1,10 @@
+package com.java_practice_code.algorithm.stack;
+
+/**
+ * 实现一个这样的栈，这个栈除了可以进行普通的push、pop操作以外，还可以进行getMin的操作，getMin方法被调用后，会返回当前栈的最小值。栈里面存放的都是 int 整数，并且数值的范围是 [-100000, 100000]。要求所有操作的时间复杂度是 O(1)。
+ * 最小栈,参考链接:https://www.cxyxiaowu.com/2968.html
+ * 实现3种解法,分别是时间复杂度o(n) + 空间复杂度o(1),时间复杂度o(1) + 空间复杂度o(n),时间复杂度o(1) + 空间复杂度o(1),
+ */
+public class MinStack {
+
+}
---
+++ src/main/java/com/java_practice_code/util/thread/DeadLockDemo.java
@@ -0,0 +1,43 @@
+package com.java_practice_code.util.thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 死锁代码展示
+ * java中传引用和传值的区别:https://www.cnblogs.com/zhangliwei/p/12056692.html
+ */
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        String lockA = "lockA";
+        String lockB = "lockB";
+
+        new Thread(new HoldLockThread(lockA, lockB), "ThreadAAA").start();
+        new Thread(new HoldLockThread(lockB, lockA), "ThreadBBB").start();
+    }
+}
+
+class HoldLockThread implements Runnable {
+
+    private String lockA;
+    private String lockB;
+
+    public HoldLockThread(String lockA, String lockB) {
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lockA) {
+            System.out.println(Thread.currentThread().getName() + "\t 自己持有:" + lockA + "\t 尝试获得:" + lockB);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lockB) {
+                System.out.println(Thread.currentThread().getName() + "\t 尝试获得:" + lockB);
+            }
+        }
+    }
+}
