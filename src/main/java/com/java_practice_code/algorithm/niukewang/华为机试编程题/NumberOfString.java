2a41d5a8dac903ccc460d99ac707c9e59f773a06
lujingxiao <lujingxiao@sythealth.com>
2020-02-18 15:27:16 +0800

    添加算法面试题

---
+++ src/main/java/com/java_practice_code/algorithm/niukewang/华为机试编程题/NumberOfString.java
@@ -0,0 +1,39 @@
+package com.java_practice_code.algorithm.niukewang.华为机试编程题;
+
+import java.util.Scanner;
+
+/**
+ * 题目描述
+ * 写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+ * 输入描述:
+ * 第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
+ * 输出描述:
+ * 输出输入字符串中含有该字符的个数。
+ * <p>
+ * 输入
+ * ABCDEF
+ * A
+ * 输出
+ * 1
+ *
+ * 已通过牛客网测试
+ */
+public class NumberOfString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            String ch = scanner.nextLine();
+            char[] chars = str.toCharArray();
+            char target = ch.toCharArray()[0];
+            int jianju = 'a' - 'A';
+            int count = 0;
+            for (char aChar : chars) {
+                if (aChar + jianju == target || aChar == target + jianju || aChar == target) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
