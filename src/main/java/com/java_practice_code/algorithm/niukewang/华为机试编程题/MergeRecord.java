a6125ad729b85bc6a5b791f4713de44936f81217
lujingxiao <lujingxiao@sythealth.com>
2020-02-20 14:27:10 +0800

    添加算法面试题

---
+++ src/main/java/com/java_practice_code/algorithm/niukewang/华为机试编程题/MergeRecord.java
@@ -0,0 +1,31 @@
+package com.java_practice_code.algorithm.niukewang.华为机试编程题;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * https://www.nowcoder.com/practice/de044e89123f4a7482bd2b214a685201?tpId=37&tqId=21231&tPage=1&rp=&ru=/ta/huawei&qru=/ta/huawei/question-ranking
+ * 数据表记录包含表索引和数值（int范围的整数），请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照key值升序进行输出
+ * 已通过牛客网测试
+ */
+public class MergeRecord {
+    private void mergeRecord() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            int n = Integer.valueOf(scanner.nextLine());
+            while (n-- > 0) {
+                String record = scanner.nextLine();
+                String[] arr = record.split(" ");
+                int index = Integer.valueOf(arr[0]);
+                int valueVal = Integer.valueOf(arr[1]);
+                map.compute(index, (key, value) -> value == null ? valueVal : value + valueVal);
+            }
+            map.forEach((key, value) -> System.out.println(key + " " + value));
+        }
+    }
+
+    public static void main(String[] args) {
+        new MergeRecord().mergeRecord();
+    }
+}
